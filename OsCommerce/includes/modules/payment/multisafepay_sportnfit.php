cad2c7f89a4f8f002b7c4b65de6572d89c0aee92
Sonny Danso <sonny.danso@multisafepay.com>
2017-03-15 11:40:03 +0100

    PLGOSC-20

--- OsCommerce/includes/modules/payment/multisafepay_sportnfit.php
+++ OsCommerce/includes/modules/payment/multisafepay_sportnfit.php
@@ -61,7 +61,7 @@ class multisafepay_sportnfit extends multisafepay {
 
     function process_button()
     {
-        return tep_draw_hidden_field('msp_paymentmethod', 'SPORTNFIT');
+        return tep_draw_hidden_field('msp_paymentmethod', 'SPORTENFIT');
     }
 
     /*
